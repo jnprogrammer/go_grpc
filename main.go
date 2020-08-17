ba8dc61869c258cecc6aa0edf83fa907095f7d84
jnprogrammer <joshuanparker@gmail.com>
2020-08-17 08:46:54 -0400

    Created simple gRPC server in go

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"google.golang.org/grpc"
+	"log"
+	"net"
+)
+
+func main() {
+	listen, err := net.listen("tcp", ":9000")
+	if err != nil {
+		log.Fatalf("Failed to listen on port 9000: %v", err)
+	}
+	grpcServer := grpc.NewServer()
+
+	if err := grpcServer.Serve(listen); err != nil {
+		log.Fatalf("Failed to server gRPC over port 9000: %v", err)
+	}
+
+}
